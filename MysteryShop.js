8ded2ee8ca25e615e2b427cee14747b630ea9526
mysterymaxim <56216341+mysterymaxim@users.noreply.github.com>
2020-05-18 12:26:33 +0300

    Add files via upload

---
+++ MysteryShop.js
@@ -0,0 +1,12 @@
+//alert("На сайте встроен мощный АнтиВирус! Так что не волнуйтесь, мы стоим на защите!");
+
+/*let element = document.querySelector("body");
+element.onclick = function() { element.style.color = 'red' };
+
+let reName = document.createElement("h1");
+element.onclick = function() { 
+    element.innerHTML = "Ха-ха-ха-ха-ха";
+
+ };
+ 
+*/
