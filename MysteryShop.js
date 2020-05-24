c61c300d21d32a42c79a8f7444714c31a8bbff95
mysterymaxim <56216341+mysterymaxim@users.noreply.github.com>
2020-05-24 10:27:10 +0300

    Final version

--- MysteryShop.js
+++ MysteryShop.js
@@ -10,3 +10,61 @@ element.onclick = function() {
  };
  
 */
+
+(function(console){
+
+	console.save = function(data, filename){
+
+		if(!data) {
+			console.error('Console.save: No data')
+			return;
+		}
+
+		if(!filename) filename = 'console.json'
+
+		if(typeof data === "object"){
+			data = JSON.stringify(data, undefined, 4)
+		}
+
+		var blob = new Blob([data], {type: 'text/json'}),
+			e    = document.createEvent('MouseEvents'),
+			a    = document.createElement('a')
+
+		a.download = filename
+		a.href = window.URL.createObjectURL(blob)
+		a.dataset.downloadurl =  ['text/json', a.download, a.href].join(':')
+		e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
+		a.dispatchEvent(e)
+	}
+})(console)
+
+saveF = function() 
+{
+	var elements = document.querySelectorAll('input, textarea');
+	for (i = 0; i < elements.length; i++) 
+	{
+	  (function(element) {
+		var id = element.getAttribute('id');
+		if(id == "click") return;
+		localStorage.setItem(id, element.value); // обязательно наличие у элементов id
+	  })(elements[i]);
+	}
+	console.save(JSON.stringify(localStorage, null,'\t'), 'data.txt');
+}
+
+document.querySelectorAll('textarea').forEach(el => {
+    el.style.height = el.setAttribute('style', 'height: ' + el.scrollHeight + 'px');
+    el.classList.add('auto');
+    el.addEventListener('input', e => {
+        el.style.height = 'auto';
+        el.style.height = (el.scrollHeight) + 'px';
+    });
+});
+
+
+
+
+
+  
+
+
